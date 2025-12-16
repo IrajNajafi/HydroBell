81bafef012035ebf912b971d5cd8ce600c936ac2
IRAJ NAJAFI <irajnajafi1988@gmail.com>
2025-12-16 12:12:38 +0330

    Setup HIlt & base\classMyApplication/manifest/MainActivity

---
+++ app/src/main/java/com/irajnajafi1988gmail/hydrobell/base/MyApplication.kt
@@ -0,0 +1,8 @@
+package com.irajnajafi1988gmail.hydrobell.base
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : Application(){
+}
