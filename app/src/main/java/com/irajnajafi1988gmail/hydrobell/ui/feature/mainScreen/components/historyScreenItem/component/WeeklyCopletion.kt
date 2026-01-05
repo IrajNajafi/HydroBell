d85ce4c62ad2791211479fe4e73c63e0d6a2a7d1
IRAJ NAJAFI <irajnajafi1988@gmail.com>
2026-01-05 11:06:14 +0330

    Cleaning up all feature/ create AlarmScreen

---
+++ app/src/main/java/com/irajnajafi1988gmail/hydrobell/ui/feature/mainScreen/components/historyScreenItem/component/WeeklyCopletion.kt
@@ -0,0 +1,153 @@
+package com.irajnajafi1988gmail.hydrobell.ui.feature.mainScreen.components.historyScreenItem.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.irajnajafi1988gmail.hydrobell.R
+import com.irajnajafi1988gmail.hydrobell.ui.feature.mainScreen.common.toColor
+import com.irajnajafi1988gmail.hydrobell.ui.feature.mainScreen.components.historyScreenItem.model.ChartBarData
+import com.irajnajafi1988gmail.hydrobell.ui.feature.mainScreen.components.homeScreenItem.model.calculateWaterState
+import com.irajnajafi1988gmail.hydrobell.ui.theme.SkyBlue
+import java.time.LocalDate
+import java.time.format.TextStyle
+import java.util.Locale
+
+@Composable
+fun WeeklyCompletion(
+    weekData: List<ChartBarData>,
+    anchorDate: LocalDate,
+) {
+    val todayLabel = anchorDate.dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.ENGLISH)
+    val scrollState = rememberScrollState()
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(200.dp)
+            .background(SkyBlue)
+            .padding(12.dp)
+    ) {
+        Column {
+            Text(
+                text = "Weekly Completion",
+                color = Color.White
+            )
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .horizontalScroll(scrollState),
+                horizontalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                weekData.forEach { item ->
+                    val isToday = item.label.equals(todayLabel, ignoreCase = true)
+
+                    ItemWeeklyCompletion(
+                        icon =  R.drawable.glass,
+                        day = item.label,
+                        isActive = isToday,
+                        value = item.value,
+                        target = item.target
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ItemWeeklyCompletion(
+    icon: Int,
+    day: String,
+    isActive: Boolean,
+    value: Int,
+    target: Int
+) {
+    val state = calculateWaterState(
+        drunkWater = value,
+        dailyWaterMl = target
+    )
+
+    val tickColor = state.toColor()
+
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Box {
+            Card(
+                modifier = Modifier.size(48.dp),
+                shape = CircleShape,
+                elevation = CardDefaults.cardElevation(4.dp),
+                colors = CardDefaults.cardColors(containerColor = Color.White)
+            ) {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Image(
+                        painter = painterResource(id = icon),
+                        contentDescription = null,
+                        modifier = Modifier.size(26.dp)
+                    )
+                }
+            }
+
+            // ✅ تیک فقط وقتی روز فعاله و نوشیدن داریم
+            if (isActive && value > 0) {
+                Card(
+                    modifier = Modifier
+                        .size(18.dp)
+                        .align(Alignment.TopEnd)
+                        .offset(x = 4.dp, y = (-4).dp),
+                    shape = CircleShape,
+                    colors = CardDefaults.cardColors(containerColor = tickColor),
+                    elevation = CardDefaults.cardElevation(2.dp)
+                ) {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Icon(
+                            painter = painterResource(R.drawable.check),
+                            contentDescription = null,
+                            tint = Color.White,
+                            modifier = Modifier.size(12.dp)
+                        )
+                    }
+                }
+            }
+        }
+
+        Spacer(Modifier.height(10.dp))
+
+        Text(
+            text = day,
+            fontSize = 16.sp,
+            color = Color.White
+        )
+    }
+}
